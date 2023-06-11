ba5b52e7b81fbb2580b166758ca752624fe8edae
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-06-11 16:56:37 +0700

    add animation

--- main.js
+++ main.js
@@ -1,47 +1,11 @@
-// // import './style.css'
-// // import javascriptLogo from "./javascript.svg";
-// import viteLogo from '/vite.svg'
-// import {setupCounter} from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-//   `
-
-// setupCounter(document.querySelector('#counter'))
-
-// document.querySelector('body').innerHTML = `
-//  <div class="container">
-//     <div class="flex flex-col justify-center items-center bg-Dark-Grayish-Blue max-w-xs lg:max-w-2xl">
-
-//     </div>
-//  </div>
-// `
-
 // Setup api & get html element
-import dice from './images/dice.svg'
-import diviDk from './images/dividerdekstop.svg'
-import diviMb from './images/dividermobile.svg'
+const main = document.querySelector('main')
 const btn = document.querySelector('button')
 const quote = document.querySelector('p')
 const id_number = document.querySelector('h1')
 const API_URL = `https://api.adviceslip.com/advice`
 async function getAdvice() {
   try {
-    let data = []
     const response = await fetch(API_URL)
     const {
       slip: {advice, id},
@@ -53,4 +17,19 @@ async function getAdvice() {
   }
 }
 getAdvice()
-btn.addEventListener('click', getAdvice)
+btn.addEventListener('click', () => {
+  // blur-md animate-pulse
+  let loading = 2500
+  let mulai = setInterval(() => {
+    main.classList.add('animate-pulse', 'blur-md')
+    btn.disabled = true
+    btn.classList.add('cursor-not-allowed')
+  }, 0)
+  setTimeout(() => {
+    getAdvice()
+    clearInterval(mulai)
+    main.classList.remove('animate-pulse', 'blur-md')
+    btn.disabled = false
+    btn.classList.remove('cursor-not-allowed')
+  }, loading)
+})
